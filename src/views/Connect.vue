3f6cf57a3230ac5ab81f8fc0a7d3c458655cbb01
sprice_hd <sprice@hansondodge.com>
2018-10-04 15:36:16 -0500

    init jira integration

---
+++ src/views/Connect.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="connect">
+    <p>This is a oauth connection page</p>
+    <AuthorizeAccount></AuthorizeAccount>
+  </div>
+</template>
+
+<script>
+import AuthorizeAccount from '@/components/AuthorizeAccount.vue'
+import OAuth from 'oauth'
+import fs from 'fs'
+
+export default {
+  name: 'connect',
+  components: {
+    AuthorizeAccount
+  },
+  mounted: function () {
+    let localURL = '172.25.9.58:8080'
+
+    let jiraInit = {
+      baseUrl: 'YOUR_JIRA_BASE_URL',
+      requestToken: '/plugins/servlet/oauth/request-token',
+      accessToken: '/plugins/servlet/oauth/access-token',
+      consumerKey: 'hdTimeTracker'
+    }
+
+    let oa = new OAuth(
+      jiraInit.baseUrl + jiraInit.requestToken,
+      jiraInit.baseUrl + jiraInit.accessToken,
+      jiraInit.consumerKey,
+      fs.readFileSync('jira.pem', 'utf8'),
+      '1.0',
+      'http://' + localURL + '/connect/callback',
+      'RSA-SHA1'
+    )
+
+    oa.getOAuthRequestToken(function (error, oauthToken, oauthTokenSecret) {
+      if (error) {
+        console.log({errorData: error.data})
+        // eslint-disable-next-line
+        response.send('Error getting OAuth access token')
+      } else {
+        // eslint-disable-next-line
+        req.session.oa = oa
+        // eslint-disable-next-line
+        req.session.oauth_token = oauthToken
+        // eslint-disable-next-line
+        req.session.oauth_token_secret = oauthTokenSecret
+        // eslint-disable-next-line
+        return res.redirect(jiraInit.baseUrl + '/plugins/servlet/oauth/authorize?oauth_token=' + oauthToken)
+      }
+    })
+  }
+}
+</script>
