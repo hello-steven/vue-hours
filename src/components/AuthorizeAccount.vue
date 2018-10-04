5a194cdc7f9fe8cc8b701e2d0a60517dec61b888
sprice_hd <sprice@hansondodge.com>
2018-10-04 15:36:16 -0500

    init jira integration

---
+++ src/components/AuthorizeAccount.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="authorize-wrapper">
+    <h1>Time Tracker - Jira Oauth</h1>
+    <p>Please sign in to Jira to experience full functionality</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AuthorizeAccount',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
