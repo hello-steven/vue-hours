470663b1504bfba2fea5f3fc0fe893137e8fd30b
Steven <stevenjacobprice@gmail.com>
2018-10-05 00:51:16 -0500

    init basic jira

--- src/components/AuthorizeAccount.vue
+++ src/components/AuthorizeAccount.vue
@@ -1,19 +1,83 @@
 <template>
   <div class="authorize-wrapper">
-    <h1>Time Tracker - Jira Oauth</h1>
-    <p>Please sign in to Jira to experience full functionality</p>
+    <h1>Jira Sign In</h1>
+    <p>Please sign in to Jira to connect the time tracker app.</p>
+    <div class="panel panel-default jira-form">
+      <label>Username</label><br>
+      <input class="form-control" type="text" name="username" id="username" placeholder="username" v-model="jiraUser"><br>
+      <label>Password</label><br>
+      <input class="form-control" type="password" name="jira_password" id="jira_password" v-model="jiraPass"><br>
+      <button type="button" :disabled="authorized === true" @click="connect()">Connect</button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'AuthorizeAccount',
-  props: {
-    msg: String
+  data () {
+    return {
+      authorized: false,
+      jiraUser: null,
+      jiraPass: null
+    }
+  },
+  mounted () {
+    // this.authorized = true
+    // let request = new Request('http://jira.hansondodge.com/rest/auth/1/session', {
+    //   method: 'get',
+    //   headers: new Headers({
+    //     'Content-Type': 'application/json'
+    //   })
+    // })
+    // fetch(request).then(function (response) {
+    //   console.log({
+    //     response: response
+    //   })
+    //   // this.$emit('jira-connect', {token: true})
+    // }).catch(function (err) {
+    //   console.log({
+    //     error: err
+    //   })
+    //   // this.authorized = false
+    // })
+  },
+  methods: {
+    connect () {
+      this.authorized = true
+      let request = new Request('http://jira.hansondodge.com/rest/api/2/myself', {
+        method: 'get',
+        mode: 'no-cors',
+        headers: new Headers({
+          'Authorization': 'Basic ' + btoa(this.jiraUser + ':' + this.jiraPass),
+          'Content-Type': 'application/json'
+        })
+      })
+      fetch(request).then(function (response) {
+        console.log({
+          response: response
+        })
+        // this.$emit('jira-connect', {token: true})
+      }).catch(function (err) {
+        console.log({
+          error: err
+        })
+        this.authorized = false
+      })
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+  .jira-form {
+    max-width: 300px;
+    padding: 1em;
+    display: block;
+  }
+  input {
+    color: #000;
+    border: 2px solid #000;
+  }
 </style>
--- src/views/Connect.vue
+++ src/views/Connect.vue
@@ -1,88 +1,16 @@
 <template>
   <div id="connect">
-    <p>This is a oauth connection page</p>
     <AuthorizeAccount></AuthorizeAccount>
   </div>
 </template>
 
 <script>
 import AuthorizeAccount from '@/components/AuthorizeAccount.vue'
-var OAuth = require('oauth')
-var fs = require('fs')
 
 export default {
   name: 'connect',
   components: {
     AuthorizeAccount
-  },
-  mounted: function () {
-    let headers = new Headers()
-    let init = { method: 'GET',
-      headers: headers,
-      mode: 'cors',
-      cache: 'default'
-    }
-    let jiraInit = {
-      baseURL: process.env.VUE_APP_JIRA_URL,
-      hostURL: process.env.VUE_APP_HOST_URL,
-      requestToken: '/plugins/servlet/oauth/request-token',
-      accessToken: '/plugins/servlet/oauth/access-token',
-      consumerKey: process.env.VUE_APP_CKEY
-    }
-
-    console.log({
-      mounted: true,
-      baseURL: jiraInit.baseURL
-    })
-
-    let oa = new OAuth(
-      jiraInit.baseURL + jiraInit.requestToken,
-      jiraInit.baseURL + jiraInit.accessToken,
-      jiraInit.consumerKey,
-      fs.readFileSync('/jira.pem', 'utf8'),
-      '1.0',
-      jiraInit.hostURL + '/connect/callback',
-      'RSA-SHA1'
-    )
-
-    let session = this.$session
-
-    oa.getOAuthRequestToken(function (error, oauthToken, oauthTokenSecret) {
-      if (error) {
-        console.log({
-          connect: true,
-          errorData: error.data
-        })
-        // eslint-disable-next-line
-        this.$router.push({path: '/', query: 'error'})
-      } else {
-        // eslint-disable-next-line
-        session.oa = oa
-        // eslint-disable-next-line
-        session.oauth_token = oauthToken
-        // eslint-disable-next-line
-        session.oauth_token_secret = oauthTokenSecret
-
-        let request = new Request(
-          jiraInit.baseURL + '/plugins/servlet/oauth/authorize?oauth_token=' + oauthToken,
-          init
-        )
-
-        fetch(request).then(function (response) {
-          console.log({
-            connect: true,
-            blob: response.blob()
-          })
-          return response.blob()
-        }).then(function (redirect) {
-          console.log({
-            connect: true,
-            redirect: redirect
-          })
-          return this.$router.push({path: '/', query: redirect})
-        })
-      }
-    })
   }
 }
 </script>
