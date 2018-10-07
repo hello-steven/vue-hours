b3be9e8d291f6f7a1dd6f710ec876e10ed9b3f6d
Steven <stevenjacobprice@gmail.com>
2018-10-07 00:26:24 -0500

    remove jira integration

--- src/router.js
+++ src/router.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
-import Connect from './views/Connect.vue'
-import Callback from './views/Callback.vue'
 
 Vue.use(Router)
 
@@ -13,16 +11,6 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
-    },
-    {
-      path: '/connect',
-      name: 'connect',
-      component: Connect
-    },
-    {
-      path: '/connect/callback',
-      name: 'callback',
-      component: Callback
     }
   ]
 })
